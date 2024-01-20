6d77306c52c502f4f668dcfe019c020daf4e5054
srafique98 <shanrafique98@gmail.com>
2024-01-19 16:46:37 -0800

    Domain layer empty classes

--- src/main/java/com/solvd/laba/domain/Atm.java
+++ src/main/java/com/solvd/laba/domain/Atm.java
@@ -1,4 +1,10 @@
 package com.solvd.laba.domain;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Atm {
+    private static final Logger LOGGER = LogManager.getLogger(Atm.class);
+
+
 }
